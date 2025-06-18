afe0e05fcf9a2837da392a2a342d89caf9fa2232
Mauricio <mauricio_cy@hotmail.com>
2025-06-17 21:17:22 -0300

    swagger and zod setup

---
+++ src/infra/http/routes/upload-image.ts
@@ -0,0 +1,26 @@
+import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import { z } from "zod/v4";
+
+export const uploadImageRoute: FastifyPluginAsyncZod = async (server) => {
+  server.post(
+    "/uploads",
+    {
+      schema: {
+        summary: "Upload an image",
+        body: z.object({
+          name: z.string(),
+          password: z.string().optional(),
+        }),
+        response: {
+          201: z.object({ uploadId: z.string() }),
+          409: z
+            .object({ message: z.string() })
+            .describe("Upload already exists"),
+        },
+      },
+    },
+    async (request, reply) => {
+      return reply.status(201).send({ uploadId: "test" });
+    }
+  );
+};
--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,9 +1,55 @@
 import fastifyCors from "@fastify/cors";
 import { fastify } from "fastify";
+import { env } from "../../env";
+import {
+  hasZodFastifySchemaValidationErrors,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import { uploadImageRoute } from "./routes/upload-image";
+import fastifyMultipart from "@fastify/multipart";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 
 const server = fastify();
 
+server.setValidatorCompiler(validatorCompiler);
+server.setSerializerCompiler(serializerCompiler);
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: "Validation error",
+      issues: error.validation,
+    });
+  }
+
+  // Após essa tratativa, enviar o erro para alguma ferramenta de observabilidade (Sentry, Datadog, Grafana/ OTel)
+
+  console.error(error);
+
+  return reply.status(500).send({ message: "Internal server error" });
+});
+
 server.register(fastifyCors, { origin: "*" });
+
+server.register(fastifyMultipart);
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Upload Server",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+server.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
+
+server.register(uploadImageRoute);
 
 server.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
   console.log("HTTP server running");
